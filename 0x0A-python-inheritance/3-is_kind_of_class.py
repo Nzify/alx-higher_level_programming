772231ad3a26fe33a99d5759c9190577cf4e5576
Nzify <maidazie@gmail.com>
2022-09-26 11:52:45 -0700

    Same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Same class or inherit from """
+
+
+def is_kind_of_class(obj, a_class):
+    """ function that returns True if the object is an instance of,
+    or if the object is an instance of a class that inherited from,
+    the specified class  """
+    return (isinstance(obj, a_class))
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
